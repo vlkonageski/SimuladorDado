dd6bf3c748d9f367ffb9ab4526a5dc8df71487ae
vlkonageski <vinicius_konageski@hotmail.com>
2021-02-08 20:37:21 -0300

    programa base

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+n = 1 
+
+while n == 1:
+    print("---MENU---\n"
+    +"1-Gerar um numero\n"
+    +"2-Finalizar programa")
+    n = int(input("Informe a opcao desejada:"))
+    if n == 1:
+        dado = random.randint(1,6)
+        print(dado)
+    elif n == 2:
+        print("Programa Finalizado!")
+        exit(0)
+    else:
+        print("Numero invalido!")
+        
